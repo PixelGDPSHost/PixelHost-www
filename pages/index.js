e2909fe7aada6701a7535e8d6f5612ec32c185a1
Connor <100000848+Coneriys@users.noreply.github.com>
2024-06-20 23:30:30 +0300

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -16,7 +16,7 @@ export default function Home() {
         <center>
             <h1 className="text-4xl font-semibold">Тарифы:</h1>
             <div className="flex w-full flex-col justify-center items-center mt-5">
-                <Tabs aria-label="Options" color="primary" variant="bordered">
+                <Tabs aria-label="Options" color="primary">
                     <Tab key="gdps" title="GDPS">
                         <Card>
                             <CardBody>
