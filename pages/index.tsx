dbcc8ae1e0867b65e8cb2d72a7b582bbc34c4357
Connor <100000848+Coneriys@users.noreply.github.com>
2024-10-15 21:55:27 +0300

    fix again

--- pages/index.tsx
+++ pages/index.tsx
@@ -9,7 +9,7 @@ import { useEffect, useState } from "react";
 export default function Home() {
   return (
     <main className="dark text-foreground prekolbg">
-      <div className="navflex z-40">Сборка от 15.10.2024 21:56</div>
+      <nav className="z-40 mt-[52px]">Сборка от 15.10.2024 21:56</nav>
       <div className="skid">
         <center>
           <h1 className="text-5xl font-semibold pixelhostbig">ByteNode</h1>
