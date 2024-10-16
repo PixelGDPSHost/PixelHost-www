37bf7fc582538870058a38de770071191a54c748
Connor <100000848+Coneriys@users.noreply.github.com>
2024-10-16 12:59:58 +0300

    Fix date

--- pages/panel/balance.tsx
+++ pages/panel/balance.tsx
@@ -140,6 +140,15 @@ export default function MyComponent() {
     checkAuth();
   }, [router]);
 
+  const convertDate = (isoDate: string): string => {
+    const date = new Date(isoDate);
+    const year = date.getFullYear();
+    const month = String(date.getMonth() + 1).padStart(2, '0');
+    const day = String(date.getDate()).padStart(2, '0');
+    
+    return `${year}-${month}-${day}`;
+  };
+
   const handlePayment = async () => {
     if (!user || !amount || !paymentSystemId) {
       console.error("Отсутствуют необходимые данные для платежа");
@@ -274,7 +283,7 @@ export default function MyComponent() {
                       : `${entry.product} | ${entry.sum}₽`}
                   </p>
                   <p className="text-small text-default-500">
-                    {entry.created_at}
+                    {convertDate(entry.created_at)}
                   </p>
                 </div>
               </CardHeader>
