087a515ab844bce6641f5e5102e5b401c995ca01
Connor <100000848+Coneriys@users.noreply.github.com>
2024-11-29 12:31:19 +0200

    111

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,95 +1,10 @@
-"use client";
-import { Inter } from "next/font/google";
 import "./globals.css";
 import { NextUIProvider } from "@nextui-org/react";
-import Link from "next/link";
-import { ToastContainer } from "react-toastify";
-import "react-toastify/dist/ReactToastify.css";
-import { useRouter } from "next/router";
-import { useState, useEffect } from "react";
-import Cookies from "js-cookie";
-import axios from "axios";
 import { ThemeProvider as NextThemesProvider } from "next-themes";
-import { Avatar } from "@/components/Components";
-
-// Define the Inter font
-const inter = Inter({ subsets: ["latin"] });
-
-// Define the type for the RootLayout props
-interface RootLayoutProps {
-  Component: React.ElementType;
-  pageProps: any;
-}
 
 export default function RootLayout({ Component, pageProps }: RootLayoutProps) {
-  const r = useRouter();
-  const [avatar, setAvatar] = useState<string | null>(null);
-
-  useEffect(() => {
-    const fetchAvatar = async () => {
-      const token = Cookies.get("PIXEL_AUTH_DO_NOT_TOUCH_THIS_NIGGA");
-      if (token) {
-        const formData = new FormData();
-        formData.append("cookie", token);
-
-        try {
-          const response = await axios.post(
-            "https://api.bytenode.cc/v1/user",
-            formData,
-            {
-              headers: {
-                "Content-Type": "multipart/form-data",
-              },
-            },
-          );
-
-          if (response.data) {
-            setAvatar(response.data.user.avatar);
-          }
-        } catch (error) {
-          console.error("Error fetching avatar:", error);
-        }
-      }
-    };
-
-    // Initial fetch
-    fetchAvatar();
-    const intervalId = setInterval(fetchAvatar, 3000);
-    return () => clearInterval(intervalId);
-  }, []);
-
-  const gotomain = () => {
-    r.push("/");
-  };
-
   return (
     <NextUIProvider>
-      <ToastContainer
-        position="bottom-right"
-        autoClose={5000}
-        hideProgressBar={false}
-        newestOnTop={false}
-        closeOnClick
-        rtl={false}
-        pauseOnFocusLoss
-        draggable
-        pauseOnHover
-        theme="dark"
-      />
-      <nav className="z-40 navflex">
-        <div className="flex navflex">
-          <img
-            src="https://cdn.bytenode.cc/pixel.png"
-            className="logo"
-            onClick={gotomain}
-            alt="Logo"
-          />
-          <p className="pixelhost cursor-pointer navflex" onClick={gotomain}>
-            ByteNode
-          </p>
-        </div>
-        <Avatar />
-      </nav>
       <NextThemesProvider attribute="class" defaultTheme="dark">
         <Component {...pageProps} />
       </NextThemesProvider>
