e181273714880885d95c0ce7f81dad04739119cb
Connor <100000848+Coneriys@users.noreply.github.com>
2024-11-29 12:32:22 +0200

    11

--- pages/_app.jsx
+++ pages/_app.jsx
@@ -2,7 +2,7 @@ import "./globals.css";
 import { NextUIProvider } from "@nextui-org/react";
 import { ThemeProvider as NextThemesProvider } from "next-themes";
 
-export default function RootLayout({ Component, pageProps }: RootLayoutProps) {
+export default function RootLayout({ Component, pageProps }) {
   return (
     <NextUIProvider>
       <NextThemesProvider attribute="class" defaultTheme="dark">
